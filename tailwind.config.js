4ec6ec96f4ff3ccaf7fe7b06a244be61e85d584c
uzair17611 <ozairkhanan@gmail.com>
2025-01-27 23:38:04 +0500

    starter

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensure Tailwind scans your source files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
